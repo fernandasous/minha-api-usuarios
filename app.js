3cb129d9915249680af35efdfbb5fce6bcb21d65
fernandasous <139650085+fernandasous@users.noreply.github.com>
2026-03-27 21:54:19 -0300

    adicionando autorização de cros e portas

--- app.js
+++ app.js
@@ -2,7 +2,21 @@ const db = require("./db")
 
 const express = require("express")
 const app = express()
-//const db = require ("./db")
+
+const path = require('path'); // importar o módulo path
+
+// express: entregar os arquivos da pasta atual
+app.use(express.static(path.join(__dirname, '.')));
+
+// Rota para entregar o index.html na página inicial
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const cors = require('cors');
+const app = express();
+app.use(cors());
+
 
 app.use (express.json())
 
